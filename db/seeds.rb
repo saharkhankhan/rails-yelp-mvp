d67e72b9cb4a23bc46d1be68639d818f364cb852
sahar khan <saharkhan1911@gmail.com>
2019-02-20 12:32:37 +0000

    routes for restaurant and reviews  added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning Data Base"
+
+puts "Creating Restaurants"
+
+restaurants = Restaurant.create([
+  {
+    name: "Dashoom",
+    address: "Shoreditch",
+    phone_number: "01436602123",
+    category: "chinese"
+  },
+  {
+    name: "Pizza Express",
+    address: "Hyde Park",
+    phone_number: "01436602123",
+    category: "italian"
+  },
+  {
+    name: "McDonalds",
+    address: "Camden",
+    phone_number: "01436602123",
+    category: "belgian"
+  },
+  {
+    name: "LeWagon",
+    address: "Hoxton",
+    phone_number: "01436602123",
+    category: "french"
+  },
+  {
+    name: "Sushi Samba",
+    address: "Bank",
+    phone_number: "01436602123",
+    category: "japanese"
+  }
+])
+
+puts "Done"
