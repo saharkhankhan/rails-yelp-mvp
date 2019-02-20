adc6da9566012631d5890b4bcb1789ce10eae2aa
sahar khan <saharkhan1911@gmail.com>
2019-02-20 13:01:49 +0000

    added index and show view

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,29 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  private
+
+  def restaurant_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
